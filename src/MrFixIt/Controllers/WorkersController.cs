1cdc0c60d40016856b2218b2f6e2280f4eff8da3
Erik Killops <erik.killops@gmail.com>
2017-02-24 09:08:43 -0800

    Refactor all html pages. Replace html helpers with tag-helpers

--- src/MrFixIt/Controllers/WorkersController.cs
+++ src/MrFixIt/Controllers/WorkersController.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MrFixIt.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MrFixIt.Controllers
 {
+    [Authorize]
     public class WorkersController : Controller
     {
         private MrFixItContext db = new MrFixItContext();
-        // GET: /<controller>/
+
         public IActionResult Index()
         {
-            var thisWorker = db.Workers.Include(i =>i.Jobs).FirstOrDefault(i => i.UserName == User.Identity.Name);
+            var thisWorker = db.Workers.Include(i => i.Jobs).FirstOrDefault(i => i.UserName == User.Identity.Name);
             if (thisWorker != null)
             {
                 return View(thisWorker);
@@ -37,7 +35,7 @@ namespace MrFixIt.Controllers
         public IActionResult Create(Worker worker)
         {
             worker.UserName = User.Identity.Name;
-            db.Workers.Add(worker); 
+            db.Workers.Add(worker);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- src/MrFixIt/ViewModels/LoginViewModel.cs
+++ src/MrFixIt/ViewModels/LoginViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MrFixIt.ViewModels
+﻿namespace MrFixIt.ViewModels
 {
     public class LoginViewModel
     {
